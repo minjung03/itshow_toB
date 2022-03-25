145a9891baa9e70851f83ecc2042a2d48562fddd
dahin <hin0258@naver.com>
2022-03-25 13:01:24 +0900

    Button Test입니다!!~

--- app/src/main/java/com/cookandroid/itshow_tob/MainActivity.java
+++ app/src/main/java/com/cookandroid/itshow_tob/MainActivity.java
@@ -3,6 +3,8 @@ package com.cookandroid.itshow_tob;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Toast.makeText(getApplicationContext(), "버튼이 눌렸습니다!", Toast.LENGTH_LONG).show();
+        Button btn = (Button)findViewById(R.id.btnTest);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "버튼이 눌렸습니다~!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Toast.makeText(getApplicationContext(), "안녕하세요~~", Toast.LENGTH_SHORT).show();
     }
 }
