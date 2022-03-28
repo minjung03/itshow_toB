a8d76827292dc15a7a86fbb240423171f0f9a364
minjung <ikwmjj@naver.com>
2022-03-28 21:38:40 +0900

    (민정) 로그인 xml 커밋

--- app/src/main/java/com/cookandroid/itshow_tob/MainActivity.java
+++ app/src/main/java/com/cookandroid/itshow_tob/MainActivity.java
@@ -13,16 +13,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button btn = (Button)findViewById(R.id.btnTest);
-        btn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "버튼이 눌렸습니다~!", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(getApplicationContext(), Search.class);
-                startActivity(intent);
-            }
-        });
-        Toast.makeText(getApplicationContext(), "안녕하세요~~", Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.login);
+
+
     }
 }
