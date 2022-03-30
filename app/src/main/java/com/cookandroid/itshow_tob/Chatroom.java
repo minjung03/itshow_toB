0d8558941f5f08cf0642ba083f33e5d83df9daa8
sim lee jin <s2011@e-mirim.hs.kr>
2022-03-30 09:35:13 +0900

    (찬희) 채팅방 xml

---
+++ app/src/main/java/com/cookandroid/itshow_tob/Chatroom.java
@@ -0,0 +1,15 @@
+package com.cookandroid.itshow_tob;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Chatroom extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chatroom);
+
+
+    }
+}
