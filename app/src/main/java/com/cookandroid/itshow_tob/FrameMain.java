864fb9dd9df893bb3809383f0d58322f570bbb9b
kimdahuin12 <hin0258@naver.com>
2022-04-03 21:18:40 +0900

    (다흰)generate frame_main.xml

---
+++ app/src/main/java/com/cookandroid/itshow_tob/FrameMain.java
@@ -0,0 +1,14 @@
+package com.cookandroid.itshow_tob;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FrameMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.frame_main);
+    }
+}
