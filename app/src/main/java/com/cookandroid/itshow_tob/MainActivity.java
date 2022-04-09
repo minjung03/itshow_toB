ac7a399649d2d4d973bd4fb34f0558aef83a6840
kimdahuin12 <hin0258@naver.com>
2022-04-09 18:58:55 +0900

    (다흰)review and bottom navigation bar

--- app/src/main/java/com/cookandroid/itshow_tob/MainActivity.java
+++ app/src/main/java/com/cookandroid/itshow_tob/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.frame_main);
+        setContentView(R.layout.review);
 /*
         new Handler().postDelayed(new Runnable() {
             @Override
