b369f3eac09a2e6dfe39f23b2cef588d2084db41
minjung Kim <ikwmjj@naver.com>
2022-04-13 14:11:19 +0900

    (민정) Add UserInfoList Item

--- app/src/main/java/com/cookandroid/itshow_tob/UserInfoAdapter.kt
+++ app/src/main/java/com/cookandroid/itshow_tob/UserInfoAdapter.kt
@@ -9,20 +9,5 @@ import android.widget.BaseAdapter
 /*
 class UserInfoAdapter(val context: Context, val userinfoList:ArrayList<UserInfoData>) : BaseAdapter() {
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view : View = LayoutInflater.from(context).inflate(R.layout.userinfo_item, null)
-    }
-
-    override fun getCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getItemId(p0: Int): Long {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getItem(p0: Int): Any {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
 }
 */
